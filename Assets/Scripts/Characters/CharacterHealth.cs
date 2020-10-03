fe26eec832da9e0c661be8cb9e7feb5c18e70eb8
ChronicHeart <42876494+ChronicHeart@users.noreply.github.com>
2020-10-02 22:02:49 -0500

    Worked on Character Health
    -Created IDamagable Interface
    - Character Health Script
    - Created Enemy Prefab
    - Created material for enemy

---
+++ Assets/Scripts/Characters/CharacterHealth.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider))]
+public class CharacterHealth : MonoBehaviour, IDamageable<int>
+{
+    #region Health
+
+    [Header("Health")]
+
+    [SerializeField]
+    protected int maxHP = 10;                                              // The maximum HP this object can start with
+    protected int m_currentHP;
+    protected int CurrentHP                                                // The accessor for current health. Use this instead of calling health directly
+    {
+        get { return m_currentHP; }
+        set { m_currentHP = Mathf.Clamp(value, 0, maxHP); }
+    }
+
+    #endregion
+
+    #region Components and References
+
+    protected Collider myCollider;                                    // The collider of this enemy
+
+    [SerializeField]
+    protected GameObject myRenderer;                                // The visual display of this enemy 
+    protected Animator animator;
+
+    #endregion
+
+    protected virtual void Start()
+    {
+        // Initialize health
+        CurrentHP = maxHP;
+
+        // Set references and components
+        myCollider = GetComponent<Collider>();
+        animator = GetComponent<Animator>();
+        if (myRenderer == null)
+        {
+            Debug.LogWarning(name + ": renderer has not been assigned!");
+        }
+    }
+
+    public virtual void Die()
+    {
+        // Disable the renderer and collider so that the enemy can no longer be interacted with
+        myRenderer.SetActive(false);
+        myCollider.enabled = false;
+    }
+
+    public virtual void HealHP(int hpRestored)
+    {
+        // Restore HP based on the parameters given
+        CurrentHP += hpRestored;
+    }
+
+    public virtual void Revive()
+    {
+        // Reset HP
+        CurrentHP = maxHP;
+
+        // Reenable the renderer and collider so that the gameobject can be interacted with again. 
+        myRenderer.SetActive(true);
+        myCollider.enabled = true;
+    }
+
+    public virtual void TakeDamage(int damageTaken)
+    {
+        // Take damage based on the given parameter
+        CurrentHP -= damageTaken;
+
+        // Kill the enemy if they have no HP
+        if (CurrentHP <= 0)
+        {
+            Die();
+        }
+    }
+}
---
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable<T>
+{
+    void TakeDamage(T damageTaken);
+
+    void HealHP(T hpRestored);
+
+    void Die();
+
+    void Revive();
+}
