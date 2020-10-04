bce48b77e2946caa65a6bba53179e350fb5e8932
ChronicHeart <42876494+ChronicHeart@users.noreply.github.com>
2020-10-04 18:33:44 -0500

    Update BossActivationScript.cs
    -The script now accepts a door to activate and deactivate

--- Assets/Scripts/Characters/Enemy/BossActivationScript.cs
+++ Assets/Scripts/Characters/Enemy/BossActivationScript.cs
@@ -6,10 +6,13 @@ public class BossActivationScript : MonoBehaviour
 {
     [SerializeField]
     BossController boss;            // The boss that we will activate
+    [SerializeField]
+    GameObject door;                // The door that closes behind the player
 
     private void Start()
     {
         boss.gameObject.SetActive(false);
+        door.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,6 +20,7 @@ public class BossActivationScript : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             boss.gameObject.SetActive(true);
+            door.SetActive(true);
         }
     }
 }
