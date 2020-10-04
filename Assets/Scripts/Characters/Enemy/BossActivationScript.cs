8b01bbe28e3484b8978d44b34f829739ec09f17b
ChronicHeart <42876494+ChronicHeart@users.noreply.github.com>
2020-10-04 18:30:57 -0500

    Created Boss Trigger
    Created boss activation script
    Created boss trigger prefab

---
+++ Assets/Scripts/Characters/Enemy/BossActivationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossActivationScript : MonoBehaviour
+{
+    [SerializeField]
+    BossController boss;            // The boss that we will activate
+
+    private void Start()
+    {
+        boss.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            boss.gameObject.SetActive(true);
+        }
+    }
+}
